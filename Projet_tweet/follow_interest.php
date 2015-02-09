19c3dc276b42d40594e329669af9d9f056a5eb61
Ayanei <Ayanei@users.noreply.github.com>
2015-02-09 16:14:28 +0100

    Reindent + meilleurs noms ID/class + retirer code css dans html

--- Projet_tweet/follow_interest.php
+++ Projet_tweet/follow_interest.php
@@ -1,62 +1,65 @@
 <?php
+//ajout du menu
 include("header.php");
+//ajout de la class
 include("model/user.php");
-
-
+//appel de méthodes, instanciation
 $follow = new user();
 $data = $follow->getSuggestUser();
-
 if(isset($_POST['continue1'])){
-
-$follow->startFollowUser();
-
+	$follow->startFollowUser();
 }
+//ouverture de session
 if(isset($_SESSION['nom'])){ 
+	?>
 
-?>
+	<div id="follow_interest_body">
 
-<body style="background:#e1e8ed">
+		<div id="header1" >
+			<div id="corps4">
+				<table>
+					<tr><td><span><img src="images/icone1.png" alt="touitteur" width="40" height="40"/></span></td>
+						<td><span class="etape">Etape 3 sur 4</span></td></tr>
+					</table>
+				</div>
+			</div>
 
-<div id="header1" >
-<div id="corps4">
-<table>
-<tr><td><span><img src="images/icone1.png" alt="touitteur" width="40" height="40"/></span></td>
-<td><span class="etape">Etape 3 sur 4</span></td></tr>
-</table>
-</div>
-</div>
+			<center><div id="corps2" >
 
-<center><div id="corps2" >
+				<div id="leftstart1">
 
-<div id="leftstart1">
+					<h3 class="suggestions">Des suggestions rien que pour vous.</h3>
+					<p class="suggestions">En se basant sur vos choix, voici quelques suggestions faites pour vous. Nous vous recommandons de toutes les suivre !</p>
 
-<h3 class="ahah2">Des suggestions rien que pour vous.</h3>
-<p class="ahah2">En se basant sur vos choix, voici quelques suggestions faites pour vous. Nous vous recommandons de toutes les suivre !</p>
+					<p class="perso"><b>Suggestions personnalisées</b><p>
 
-<p class="ahah"><b>Suggestions personnalisées</b></<p>
+						<div id="gcontact" >
+							<form action="follow_interest.php" method="post" >
+								<?php foreach($data as $v) { 
+									?>
+									<div id="contact">
 
-<div id="gcontact" >
-<form action="follow_interest.php" method="post" >
-<?php foreach($data as $v) { 
+										<p><?php echo $v['login'] . " " . $v['mail']; ?></span><span class="close"><input type="checkbox" checked="checked" name="checkbox1[]" value="<?php echo $v['id_user']; ?>"/><p>
 
-?>
-	<div id="contact">
+										</div>
+										<?php } ?>
 
-	<p><?php echo $v['login'] . " " . $v['mail']; ?></span><span class="close"><input type="checkbox" checked="checked" name="checkbox1[]" value="<?php echo $v['id_user']; ?>"/><p>
+									</div>
 
-	</div>
-<?php } ?>
+								</div>
+								<div id="rightstart1">
 
-</div>
+									<input type="submit" value="Suivez ces comptes et continuez" name="continue1" id="submit6"/>
+								</form>
+							</div>
+						</div>
+					</div></center>
+					<script src ="jquery.js" type ="text/javascript"></script>
+					<script src ="inscription.js" type ="text/javascript"></script>
 
-</div>
-<div id="rightstart1">
-
-<input type="submit" value="Suivez ces comptes et continuez" name="continue1" id="submit6"/>
-</form>
-</div>
-</div></center>
-<script src = "jquery.js" type = "text/javascript"></script>
-<script src = "inscription.js" type = "text/javascript"></script>
-
-<?php }else {  header("location:index.php"); }  ?>
+					<?php 
+				}
+				else 
+					{  //si la connexion à échouée redirection vers l'accueil
+					header("location:index.php"); 
+				}  ?>
