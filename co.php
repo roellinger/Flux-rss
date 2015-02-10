b77934a7feac3a590d545acf3f1a6ebd33fa84b5
Ayanei <Ayanei@users.noreply.github.com>
2015-02-10 16:14:09 +0100

    Mise aux normes

--- co.php
+++ co.php
@@ -3,16 +3,16 @@ include("model/user.php");
 
 if(isset($_POST['submit'])){
 
-$inscription = new user();
-$inscription->connexion($_POST['mail'], $_POST['mdp']);
+	$inscription = new user();
+	$inscription->connexion($_POST['mail'], $_POST['mdp']);
 
 }
- ?>
+?>
 
 
 <div id="header" >
 
-<span><a href="index.php"><img style="margin-left:550px" src="images/icone.png" alt="touitteur" width="50" height="50"/></a></span>
+	<span><a href="index.php"><img style="margin-left:550px" src="images/icone.png" alt="touitteur" width="50" height="50"/></a></span>
 
 </div>
 
@@ -20,17 +20,17 @@ $inscription->connexion($_POST['mail'], $_POST['mdp']);
 <div id="corps" >
 
 
-<h2>Se connecter a Touitteur</h2>
-<form id="lol" action="co.php" method="post">
+	<h2>Se connecter a Touitteur</h2>
+	<form action="co.php" method="post">
 
-<input class="grand" type="texte" name="mail" placeholder="Rentrez votre adresse mail" /><br>
-<input class="grand"  ype="password" name="mdp"  placeholder="Rentrez votre mot de passe" /><br>
-<input type="submit" name="submit" id="submit" />
+		<input class="grand" type="texte" name="mail" placeholder="Rentrez votre adresse mail" /><br>
+		<input class="grand"  ype="password" name="mdp"  placeholder="Rentrez votre mot de passe" /><br>
+		<input type="submit" name="submit" id="submit" />
 
-</form>
+	</form>
 
-<div id="bottom">
+	<div id="bottom">
 
-<p>Nouveau sur Twitter ? <a href="inscription.php">S'inscrire maintenant </a>
-</div>
-</div>
+		<p>Nouveau sur Twitter ? <a href="inscription.php">S'inscrire maintenant </a>
+		</div>
+	</div>
