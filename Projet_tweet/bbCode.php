bf3f8da6003a7da6ed1ecca36dd5916292ba65f6
roellinger <roellinger.robin@gmail.com>
2015-02-10 15:34:05 +0100

    Revert "Y"
    This reverts commit b7776fc6f75c7949e5ad83e65e448325bb7f33be.

--- Projet_tweet/bbCode.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function bbCode($article)
-{
-$article= preg_replace( "#\[b\](.*)\[/b\]#si", "<span class = \"gras\" >$1</span>",$article);
- $article = preg_replace( "#\[i\](.*)\[/i\]#si", "<span class = \"italique\" >$1</span>",$article); 
- $article= preg_replace( "#\[s\](.*)\[/s\]#si", "<span class = \"souligne\" >$1</span>",$article);
-
-?>
